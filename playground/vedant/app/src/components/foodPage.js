83c143e1951803c6f11987aded183526c2288977
zaffrew <vedantsrin@gmail.com>
2019-11-19 17:34:49 -0500

    Added empty foodPage file to test branch

---
+++ playground/vedant/app/src/components/foodPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text, View} from "react-native";
+import styles from "./styles";
+
+export default class Home extends React.Component {
+    render() {
+        return (
+                null
+        );
+    }
+}
