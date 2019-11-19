759a2986308fe653e0712ce9e085ba4cc398972a
zaffrew <vedantsrin@gmail.com>
2019-11-19 18:34:41 -0500

    Modified food page design

--- playground/vedant/app/src/components/foodPage.js
+++ playground/vedant/app/src/components/foodPage.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import {Text, View} from "react-native";
 import styles from "./styles";
+import DeadSpaceStatusBar from './deadSpaceStatusBar';
+import Tabs from './Tabs.js';
+
+const foodName = "Curry";
 
 export default class Home extends React.Component {
+
+    state = {
+        foodName: foodName
+    }
+
     render() {
         return (
-                null
+                <View style={styles.container}>
+                    <DeadSpaceStatusBar/>
+                    <Text style={styles.heading}>{this.state.foodName}</Text>
+                    <Tabs/>
+                </View>
         );
     }
 }
