71ff7481adcafe33d0aaeed6495133b56f77e447
Ian McClean <ianmcclean@Ians-MacBook-Pro.local>
2020-01-01 18:46:54 +0100

    Add basic layout for user page

--- app/src/components/mainTabs/UserPage.js
+++ app/src/components/mainTabs/UserPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {AsyncStorage, View} from "react-native";
-import {Avatar, Subheading} from 'react-native-paper';
+import {Avatar, IconButton, Subheading, Surface} from 'react-native-paper';
+import styles from '../../../settings/styles'
+import SafeView from "../SafeView";
+import {List} from 'react-native-paper';
 
 
 export default class extends React.Component {
@@ -13,9 +16,9 @@ export default class extends React.Component {
     async componentDidMount() {
         const username = await AsyncStorage.getItem('username');
 
-        let avatar
+        let avatar;
         if (username) {
-            const initials = username.split(" ").filter(word => word.length != 0).map(word => word[0]).reduce((sum, next) => sum += next, "")
+            const initials = username.split(" ").filter(word => word.length != 0).map(word => word[0]).reduce((sum, next) => sum += next, "");
             avatar = <Avatar.Text label={initials.toUpperCase()}/>
         } else {
             avatar = <Avatar.Icon icon="account"/>
@@ -26,10 +29,41 @@ export default class extends React.Component {
 
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                {this.state.avatar}
-                <Subheading>{this.state.username}</Subheading>
-            </View>
+            <SafeView style={{flex: 1, flexDirection: 'column', alignItems: 'stretch', justifyContent: 'flex-start'}}>
+                <Surface
+                    style={{flex: 0, padding: 8, elevation: 4, alignItems: 'center', justifyContent: 'flex-start'}}>
+                    {this.state.avatar}
+                    <Subheading>{this.state.username}</Subheading>
+                </Surface>
+                <Surface
+                    style={{
+                        flex: 0,
+                        padding: 8,
+                        elevation: 4,
+                        alignItems: 'stretch',
+                        justifyContent: 'flex-start'
+                    }}>
+                    <List.Section>
+                        {getListItem('Edit Username', 'account')}
+                        {getListItem('Payment Information', 'currency-usd')}
+                        {getListItem('Help', 'help-rhombus')}
+                        {getListItem('Logout', 'logout')}
+                    </List.Section>
+                </Surface>
+
+            </SafeView>
         )
     }
+}
+
+
+function getListItem(title, iconName) {
+    return (
+        <List.Item
+            title={title}
+            left={(props) => {
+                return <IconButton icon={iconName}/>
+            }}
+        />
+    )
 }
