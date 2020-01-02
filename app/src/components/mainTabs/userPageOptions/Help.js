d62c85cf2326f1bdde39855686edb7f7aeefa9e9
Ian McClean <ianmcclean@Ians-MacBook-Pro.local>
2020-01-02 21:35:26 +0100

    Added temp pages for user page

---
+++ app/src/components/mainTabs/userPageOptions/Help.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View} from "react-native";
+import {Text} from "react-native-paper";
+
+export default class Help extends React.Component {
+    render() {
+        return (
+            <View style={styles.centeredContainer}>
+                <Text>Help Page</Text>
+            </View>
+        );
+    }
+}
