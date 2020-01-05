ff1a871dadc72e0d00981169beda67b784e352bb
Ian McClean <ianmcclean@Ians-MacBook-Pro.local>
2020-01-05 21:56:49 +0100

    Implemented Redux

--- app/src/components/mainTabs/Home.js
+++ app/src/components/mainTabs/Home.js
@@ -5,31 +5,18 @@ import SidewaysScroll from "../SidewaysScroll";
 import SafeView from '../SafeView'
 import {Title} from "react-native-paper";
 
-export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: ''
-        }
-    }
+import {connect} from 'react-redux'
 
-    async componentDidMount() {
-        const username = await AsyncStorage.getItem('username');
-        this.setState({username})
-    }
 
-    render() {
-        return (
-            <SafeView style={styles.container}>
-                <Title style={{padding: 5, fontSize: 30}}>Hello {this.state.username}!</Title>
-                <ScrollView style={styles.container}>
-                    <SidewaysScroll title={"Taste Breakers"}/>
-                    <SidewaysScroll title={"Popular Near You"}/>
-                    <SidewaysScroll title={"Top Ten This Week"}/>
-                    <SidewaysScroll title={"Recent Meals"}/>
-                    <SidewaysScroll title={"Pantry to Plate"}/>
-                </ScrollView>
-            </SafeView>
-        );
-    }
-}
+export default connect((state) => ({username: state.username}))((props) => (
+    <SafeView style={styles.container}>
+        <Title style={{padding: 5, fontSize: 30}}>Hello {props.username}!</Title>
+        <ScrollView style={styles.container}>
+            <SidewaysScroll title={"Taste Breakers"}/>
+            <SidewaysScroll title={"Popular Near You"}/>
+            <SidewaysScroll title={"Top Ten This Week"}/>
+            <SidewaysScroll title={"Recent Meals"}/>
+            <SidewaysScroll title={"Pantry to Plate"}/>
+        </ScrollView>
+    </SafeView>
+));
