8cf764b6c98de76d36acdc82378fab37b2b0bd8e
Ian McClean <ianmcclean@Ians-MacBook-Pro.local>
2020-01-11 17:00:11 -0500

    Fixed navigator bug where food page would not navigate

--- app/src/components/HomeNavigator.js
+++ app/src/components/HomeNavigator.js
@@ -7,15 +7,13 @@ import React from "react";
 
 const HomeStack = createStackNavigator();
 
-export default class AppNavigator extends React.Component {
-
+export default class HomeNavigator extends React.Component {
     render() {
         return (
-                <HomeStack.Navigator headerMode={"none"} initialRouteName="Home">
-                    <HomeStack.Screen name="Home" component={Home}/>
-                    <HomeStack.Screen name="Food" component={Food}/>
-                </HomeStack.Navigator>
-
+            <HomeStack.Navigator headerMode={"none"} initialRouteName="Home">
+                <HomeStack.Screen name="Home" component={Home}/>
+                <HomeStack.Screen name="Food" component={Food}/>
+            </HomeStack.Navigator>
         )
     }
 
