b2407012172e8efa94aef0c13007a9249bb83606
Ian McClean <ianmcclean@Ians-MacBook-Pro.local>
2020-01-12 13:31:37 -0500

    Change Home to reolve merge conflict

--- app/src/components/mainTabs/Home.js
+++ app/src/components/mainTabs/Home.js
@@ -5,19 +5,9 @@ import SidewaysScroll from "../SidewaysScroll";
 import SafeView from '../SafeView'
 import {Title} from "react-native-paper";
 
-export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: ''
-        }
-    }
+export default connect((state) => ({username: state.username}))(Home);
 
-    async componentDidMount() {
-        const username = await AsyncStorage.getItem('username');
-        this.setState({username})
-    }
-
+class Home extends React.Component {
     openFood = () => {
         console.log("Entered function");
         this.props.navigation.navigate('Food');
