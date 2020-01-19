eba7df00c842d076cc824d85db550b54082d0033
ianmcclean <ianmcclean17@gmail.com>
2020-01-19 12:44:23 -0500

    Added documentation to withProps

--- app/src/components/withProps.js
+++ app/src/components/withProps.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 /**
- * Applies the passed params to the normal list of params.
+ * Applies the default params to the end of the normal list of params.
+ * This means that if a default param has the same name as a normal param, the normal param will used.
  */
 export default (WrappedComponent, defaultProps) => (props => <WrappedComponent {...{...defaultProps, ...props}}/>)
