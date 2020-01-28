3b9b53b865ec6346832fa9fb8c133f031543d036
Vedant Srinivasan Kartik Srinivasan <vedantsrin@gmail.com>
2020-01-28 16:43:55 -0500

    Made basic layout for search page. Fixed InteractiveTextImage to support custom sizing.

--- app/src/components/SidewaysScroll.js
+++ app/src/components/SidewaysScroll.js
@@ -16,7 +16,9 @@ export default class SidewaysScroll extends React.Component {
         super(props);
         this.state = {
             title: this.props.title,
-            onTap: this.props.onTap
+            onTap: this.props.onTap,
+            height: this.props.height,
+            width: this.props.width,
         }
     }
 
@@ -25,14 +27,15 @@ export default class SidewaysScroll extends React.Component {
             <View>
                 <Text style={styles.subtitle}>{this.state.title}</Text>
                 <ScrollView contentContainerStyle={{height: 250}} horizontal={true} alwaysBounceHorizontal={true} showsHorizontalScrollIndicator={false}>
-                    <InteractiveTextImage image={curry} text='Curry' onTap={this.state.onTap}/>
-                    <InteractiveTextImage image={applePie} text='Apple Pie' onTap={this.state.onTap}/>
-                    <InteractiveTextImage image={bananaBread} text='Banana Bread' onTap={this.state.onTap}/>
-                    <InteractiveTextImage image={beans} text='Beans' onTap={this.state.onTap}/>
-                    <InteractiveTextImage image={pasta} text='Pasta' onTap={this.state.onTap}/>
-                    <InteractiveTextImage image={sushi} text='Sushi' onTap={this.state.onTap}/>
+                    <InteractiveTextImage image={curry} text='Curry' onTap={this.state.onTap} height={this.state.height} width={this.state.width} textSize={20}/>
+                    <InteractiveTextImage image={applePie} text='Apple Pie' onTap={this.state.onTap} height={this.state.height} width={this.state.width} textSize={20}/>
+                    <InteractiveTextImage image={bananaBread} text='Banana Bread' onTap={this.state.onTap} height={this.state.height} width={this.state.width} textSize={20}/>
+                    <InteractiveTextImage image={beans} text='Beans' onTap={this.state.onTap} height={this.state.height} width={this.state.width} textSize={20}/>
+                    <InteractiveTextImage image={pasta} text='Pasta' onTap={this.state.onTap} height={this.state.height} width={this.state.width} textSize={20}/>
+                    <InteractiveTextImage image={sushi} text='Sushi' onTap={this.state.onTap} height={this.state.height} width={this.state.width} textSize={20}/>
                 </ScrollView>
             </View>
         );
     }
 }
+
