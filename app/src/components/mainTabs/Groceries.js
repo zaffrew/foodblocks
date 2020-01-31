5621967db5817bf5470d8a3941828410788b5ff9
lia-mason <lia.k.mason@gmail.com>
2020-01-31 01:58:07 -0500

    Added groceries page

--- app/src/components/mainTabs/Groceries.js
+++ app/src/components/mainTabs/Groceries.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, StyleSheet} from 'react-native'
+import { TextInput } from 'react-native-paper';
+import colors from '../../../settings/colors'
+import SafeView from '../SafeView'
 
 import styles from "../../../settings/styles"
 
 export default class Groceries extends React.Component {
+
+    state = {
+        text: ''
+      };
+
     render() {
         return (
-            <View style={styles.centeredContainer}>
-                <Text>Groceries</Text>
-            </View>
+            <SafeView style={[styles.container, {backgroundColor: colors.foodblocksRed}]}>
+                <TextInput
+                    mode='outlined'
+                    placeholder='Add item'
+                    value={this.state.text}
+                    onChangeText={text => this.setState({ text })}
+                />
+            </SafeView>
         );
     }
 }
