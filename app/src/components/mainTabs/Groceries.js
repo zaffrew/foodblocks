b4571e00ce2c9c573c564db9e8fdb009fead6c53
lia-mason <lia.k.mason@gmail.com>
2020-02-05 18:01:24 -0500

    Added example list

--- app/src/components/mainTabs/Groceries.js
+++ app/src/components/mainTabs/Groceries.js
@@ -14,31 +14,81 @@ export default class Groceries extends React.Component {
 
     render() {
         return (
-            <SafeView style={[styles.container, {backgroundColor: colors.foodblocksRed}]}>
+            <View>
+            <SafeView style={{flex: 1/4}, {backgroundColor: colors.foodblocksRed}}>
+                <View style={{flex:1},{height: 200},{backgroundColor: colors.foodblocksRed}}>
                 <Headline style={[{color: 'white'}, {paddingVertical: 5}, {paddingHorizontal: 10}]}> Grocery List</Headline>
                 <TextInput
-                    style={{paddingVertical:5}, {paddingHorizontal:20}}
+                    style={[{paddingVertical:5}, {paddingHorizontal:20}, {paddingBottom:20}]}
                     mode='outlined'
                     placeholder='Add item'
                     value={this.state.text}
                     onChangeText={text => this.setState({ text })}
                 />
-                <View style={styles.listContainer}>
-                    <List.Item
-                        title="Milk"
-                        titleStyle={{color:'white'}}
-                        left={props => <List.Icon color={Colors.white} icon="square-outline" />}
-                    />
                 </View>
             </SafeView>
+                    <List.Item
+                        title="Milk"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                    <List.Item
+                        title="Eggs"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                    <List.Item
+                        title="Bread"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                    <List.Item
+                        title="Chicken"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                    <List.Item
+                        title="Carrots"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                     <List.Item
+                        title="Apples"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                     <List.Item
+                        title="Butter"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+                    <List.Item
+                        title="Crackers"
+                        style={{paddingVertical:0}}
+                        left={props => <List.Icon color={Colors.black} icon="square-outline" />}
+                        right={props => <List.Icon color={Colors.grey400} icon="menu" />}
+                    />
+            </View>
         );
     }
 }
 
 const listStyles = StyleSheet.create({
     listContainer: {
-      flex: 1,
-      backgroundColor: '#fff',
+        flex : 2/4,
+        backgroundColor: 'white',
+        paddingVertical: 0,
+        paddingTop: 0,
+        shadowOffset:{  width: 0,  height: 5,  },
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
     }
 });
 
