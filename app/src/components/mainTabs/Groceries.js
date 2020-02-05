664739868729f979a1ade2bd327eeac39a90ea90
lia-mason <lia.k.mason@gmail.com>
2020-02-05 15:45:12 -0500

    Changed add items bar

--- app/src/components/mainTabs/Groceries.js
+++ app/src/components/mainTabs/Groceries.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Text, View, StyleSheet} from 'react-native'
-import { TextInput } from 'react-native-paper';
+import { TextInput, Headline} from 'react-native-paper';
 import colors from '../../../settings/colors'
 import SafeView from '../SafeView'
 
@@ -15,7 +15,9 @@ export default class Groceries extends React.Component {
     render() {
         return (
             <SafeView style={[styles.container, {backgroundColor: colors.foodblocksRed}]}>
+                <Headline style={[{color: 'white'}, {paddingVertical: 5}, {paddingHorizontal: 10}]}> Grocery List</Headline>
                 <TextInput
+                    style={{paddingVertical:5}, {paddingHorizontal:20}}
                     mode='outlined'
                     placeholder='Add item'
                     value={this.state.text}
