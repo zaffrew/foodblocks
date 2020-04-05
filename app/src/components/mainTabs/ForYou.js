50d68f2b99f6bb154b7112d8b6fbeee04cd55dbe
ianmcclean <ianmcclean17@gmail.com>
2020-04-04 21:17:11 -0400

    Cleaned up ForYou.js code
    If code is copy-pasted, as with the screens/buttons, it is usually a good sign that the code can be refractored into a function.
    
    Additionally, it is best to avoid the use of absolute positioning, and use flexbox to position elements instead.

--- app/src/components/mainTabs/ForYou.js
+++ app/src/components/mainTabs/ForYou.js
@@ -1,235 +1,79 @@
-import * as React from 'react';
-import { Appbar, List, getListItem, Title, Headline, configureFonts, Button } from 'react-native-paper';
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from 'react-native-chart-kit'
+import React from 'react';
+import {View} from 'react-native'
+import {Button} from 'react-native-paper';
+import {Dimensions, StyleSheet, Text} from 'react-native';
+import {BarChart, PieChart} from 'react-native-chart-kit'
 import SafeView from '../SafeView'
 import colors from '../../../settings/colors'
 import {createStackNavigator} from "@react-navigation/stack";
 
+const categories = [
+    'Calories',
+    'Vitamin A',
+    'Vitamin B',
+    'Vitamin C',
+    'Total Fat',
+    'Iron'
+]
+
 class MyComponent extends React.Component {
-  render() {
-    return (
-        <SafeView style={{backgroundColor: colors.foodblocksRed}}>
-        <Text  style={{padding: 5, fontSize: 50, color: "white", textAlign:'center',}}>Today</Text>
-        <PieChart
-          data={data}
-          width={screenWidth}
-          height={220}
-          chartConfig={chartConfigCircle}
-          accessor="population"
-          backgroundColor="transparent"
-          paddingLeft="15"
-          absolute
-        />
-    <Button onPress = {() => this.props.navigation.navigate("Calories")}
-    style={styles.c}
-    >
-      Calories
-    </Button>
-    <Button onPress = {() => this.props.navigation.navigate("vitamanA")}
-    style={styles.va}
-    >
-      Vitman A
-    </Button>
-    <Button onPress = {() => this.props.navigation.navigate("vitamanB")}
-    style={styles.vb}
-    >
-      Vitman B
-    </Button>
-    <Button onPress = {() => this.props.navigation.navigate("vitamanC")}
-    style={styles.vc}
-    >
-      Vitman C
-    </Button>
-    <Button onPress = {() => this.props.navigation.navigate("totalFat")}
-    style={styles.f}
-    >
-      Total fat
-    </Button>
-    <Button onPress = {() => this.props.navigation.navigate("iron")}
-    style={styles.i}
-    >
-      Iron
-    </Button>
-    </SafeView>
-    
-    );
-  }
-}
-class Calories extends React.Component{
-  render(){
-    return(
-      <SafeView>
-        <Text  style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign:'center',}}>Calories</Text>
-        <BarChart
-    style={styles.bottom}
-    data={barData}
-    width={screenWidth} // screenWidth
-    height={220}
-    yAxisLabel={''}
-    chartConfig={chartConfigBar}
-    /> 
+    render() {
+        const buttons = categories.map((name, i) =>
+            <Button key={i} onPress={() => this.props.navigation.navigate(name)}>
+                {name.toUpperCase()}
+            </Button>
+        )
 
-      </SafeView>
+        return (
+            <View style={{flex: 1}}>
+                <SafeView style={{backgroundColor: colors.foodblocksRed}}>
+                    <Text style={{padding: 5, fontSize: 50, color: "white", textAlign: 'center',}}>
+                        Today
+                    </Text>
+                    <PieChart
+                        data={data}
+                        width={screenWidth}
+                        height={220}
+                        chartConfig={chartConfigCircle}
+                        accessor="population"
+                        backgroundColor="transparent"
+                        paddingLeft={15}
+                    />
+                </SafeView>
+                <View style={{flex: 1, justifyContent: 'space-around'}}>
+                    {buttons}
+                </View>
+            </View>
+        );
+    }
+}
+
+function getPage(name) {
+    return () => (
+        <SafeView>
+            <Text style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign: 'center',}}>{name}</Text>
+            <BarChart
+                data={barData}
+                width={screenWidth} // screenWidth
+                height={220}
+                yAxisLabel={''}
+                chartConfig={chartConfigBar}
+            />
+        </SafeView>
     )
-  }
+
 }
 
-class vitamanA extends React.Component{
-  render(){
-    return(
-      <SafeView >
-        <Text  style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign:'center',}}>Vitaman A</Text>
-        <BarChart
-    style={styles.bottom}
-    data={barData}
-    width={screenWidth} // screenWidth
-    height={220}
-    yAxisLabel={''}
-    chartConfig={chartConfigBar}
-    /> 
-
-      </SafeView>
-    )
-  }
-}
-
-class vitamanB extends React.Component{
-  render(){
-    return(
-      <SafeView>
-        <Text  style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign:'center',}}>Vitaman B</Text>
-        <BarChart
-    style={styles.bottom}
-    data={barData}
-    width={screenWidth} // screenWidth
-    height={220}
-    yAxisLabel={''}
-    chartConfig={chartConfigBar}
-    /> 
-
-      </SafeView>
-    )
-  }
-}
-
-class vitamanC extends React.Component{
-  render(){
-    return(
-      <SafeView>
-        <Text  style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign:'center',}}>Vitaman C</Text>
-        <BarChart
-    style={styles.bottom}
-    data={barData}
-    width={screenWidth} // screenWidth
-    height={220}
-    yAxisLabel={''}
-    chartConfig={chartConfigBar}
-    /> 
-
-      </SafeView>
-    )
-  }
-}
-
-class iron extends React.Component{
-  render(){
-    return(
-      <SafeView>
-        <Text  style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign:'center',}}>Iron</Text>
-        <BarChart
-    style={styles.bottom}
-    data={barData}
-    width={screenWidth} // screenWidth
-    height={220}
-    yAxisLabel={''}
-    chartConfig={chartConfigBar}
-    /> 
-
-      </SafeView>
-    )
-  }
-}
-
-class totalFat extends React.Component{
-  render(){
-    return(
-      <SafeView>
-        <Text  style={{padding: 5, fontSize: 50, color: "#E62D1B", textAlign:'center',}}>Total Fat</Text>
-        <BarChart
-    style={styles.bottom}
-    data={barData}
-    width={screenWidth} // screenWidth
-    height={220}
-    yAxisLabel={''}
-    chartConfig={chartConfigBar}
-    /> 
-
-      </SafeView>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  c: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: -50,
-  },
-  bottom: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      bottom: -225,
-  },
-  va: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: -100,
-  },
-  vb: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: -150,
-  },
-  vc: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: -200,
-  },
-  f: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: -250,
-  },
-  i: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: -300,
-  }
-});
 const barData = {
     labels: ['Sunday', 'Monday', 'Tuesday', 'Wedensday', 'Thursday', 'Friday', 'Saturday'],
     datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43, 25],
-      },
+        {
+            data: [20, 45, 28, 80, 99, 43, 25],
+        },
     ],
-  };
-  const chartConfigBar = {
+};
+
+const chartConfigBar = {
     backgroundGradientFrom: "#E43938",
     backgroundGradientFromOpacity: 0.9,
     backgroundGradientTo: "#E62D1B",
@@ -237,52 +81,53 @@ const barData = {
     color: (opacity = 1) => `rgba(25, 125, 425, ${opacity})`,
     strokeWidth: 2, // optional, default 3
     barPercentage: 0.5,
-  };
-  const data = [
+};
+
+const data = [
     {
-      name: "Calories",
-      population: 21500000,
-      color: "rgba(131, 167, 234, 1)",
-      legendFontColor: "white",
-      legendFontSize: 12
+        name: "Calories",
+        population: 21500000,
+        color: "rgba(131, 167, 234, 1)",
+        legendFontColor: "white",
+        legendFontSize: 12
     },
     {
-      name: "Vitaman A",
-      population: 2800000,
-      color: "blue",
-      legendFontColor: "white",
-      legendFontSize: 12
+        name: "Vitaman A",
+        population: 2800000,
+        color: "blue",
+        legendFontColor: "white",
+        legendFontSize: 12
     },
     {
-      name: "Vitaman B",
-      population: 527612,
-      color: "green",
-      legendFontColor: "white",
-      legendFontSize: 12
+        name: "Vitaman B",
+        population: 527612,
+        color: "green",
+        legendFontColor: "white",
+        legendFontSize: 12
     },
     {
-      name: "Vitamin C",
-      population: 8538000,
-      color: "orange",
-      legendFontColor: "white",
-      legendFontSize: 12
+        name: "Vitamin C",
+        population: 8538000,
+        color: "orange",
+        legendFontColor: "white",
+        legendFontSize: 12
     },
     {
-      name: "Total Fat",
-      population: 2500000,
-      color: "yellow",
-      legendFontColor: "white",
-      legendFontSize: 12
+        name: "Total Fat",
+        population: 2500000,
+        color: "yellow",
+        legendFontColor: "white",
+        legendFontSize: 12
     },
     {
-      name: "Iron",
-      population: 11920000,
-      color: "purple",
-      legendFontColor: "white",
-      legendFontSize: 12
+        name: "Iron",
+        population: 11920000,
+        color: "purple",
+        legendFontColor: "white",
+        legendFontSize: 12
     }
-  ];
-  const chartConfigCircle = {
+];
+const chartConfigCircle = {
     backgroundGradientFrom: "#1E2923",
     backgroundGradientFromOpacity: 0,
     backgroundGradientTo: "#08130D",
@@ -290,24 +135,21 @@ const barData = {
     color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
     strokeWidth: 2, // optional, default 3
     barPercentage: 0.5
-  };
+};
 
-  const screenWidth = Dimensions.get("window").width;
+const screenWidth = Dimensions.get("window").width;
 
-  const Stack = createStackNavigator();
-  const statsNavigator = (props) => {
-  return (
-      <Stack.Navigator initialRouteName="MyComponent"
-                       screenOptions={{headerTitle: null, headerBackTitleVisible: false,}}>
-          <Stack.Screen options={{headerShown: false}} name="MyComponent"
-                        component={(MyComponent)}/>
-          <Stack.Screen name="Calories" component={Calories}/>
-          <Stack.Screen name="vitamanA" component={vitamanA}/>
-          <Stack.Screen name="vitamanB" component={vitamanB}/>
-          <Stack.Screen name="vitamanC" component={vitamanC}/>
-          <Stack.Screen name="iron" component={iron}/>
-          <Stack.Screen name="totalFat" component={totalFat}/>
-      </Stack.Navigator>
-  )
-  }
-  export default statsNavigator;
+const Stack = createStackNavigator();
+const statsNavigator = (props) => {
+    const screens = categories.map((name, i) => <Stack.Screen key={i} name={name} component={getPage(name)}/>)
+
+    return (
+        <Stack.Navigator initialRouteName="MyComponent"
+                         screenOptions={{headerTitle: null, headerBackTitleVisible: false,}}>
+            <Stack.Screen options={{headerShown: false}} name="MyComponent"
+                          component={(MyComponent)}/>
+            {screens}
+        </Stack.Navigator>
+    )
+}
+export default statsNavigator;
